675493c335c5f6eac26e6b5ea193186a46b7c7df
Olivier Sigaud <Olivier.Sigaud@upmc.fr>
2018-08-30 14:29:37 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# rl_labs_notebooks
-Labs for understanding and coding Standard Reinforcement Learning concepts
+# Jupyter Notebook Lab on the Basics of Reinforcement Learning
+
+This is a private repository for setting up a basic reinforcement learning labs based on jupyter notebooks.
+
+The labs are intended for being available to anyone through internet, so the assignments should be self-explanatory.
+
+## Getting Started
+
+### Download 
+
+Get this repository using
+
+```
+git clone https://github.com/Yasmine-H/RL-Basics-Jupyter-Notebook.git
+```
+
+### Prerequisites
+
+* Python 3.6
+* Jupyter Notebook
+* numpy
+* matplotlib
+* ipynb
+
+### Installation
+
+```
+pip install -r requirements.txt
+
+sudo updatedb
+
+path1=$(locate new_ipynb_utils/utils.py)
+
+path2=$(locate ipynb/utils.py)
+
+cat "$path2" > "$path1"
+
+```
+
+Note : In order to enable the imports between notebooks, we used the [ipynb library](https://github.com/ipython/ipynb). 
+
+The syntax to import a function, say sarsa from the reinforcement_learning notebook, is as follows:
+
+``` 
+from ipynb.fs.defs.reinforcement_learning import sarsa
+```
+
+### Code
+
+
+<br> 
+
+There are 10 *.ipynb files to open on Jupyter Notebook.
+
+* lab_instructions.ipynb : Summary of the lab. It contains the links to the different assignments, from Dynamic Programming to Reinforcement Learning algorithms. This is the file you will refer to when doing these labs.
+
+* mdp.ipynb contains 4 classes :
+
+		- maze : used to describe a maze like environment 
+		
+		- simple_actspace : used to describe an action space
+		
+		- mdp : generic class used to define a Markov Decision Process
+
+		- maze_mdp : a class used to define a maze like Markov Decision Process
+
+* maze_plotter.ipynb : used to show the states value functions and policies processed by the algorithms as well as the agent postion in RL algorithms.
+
+* toolbox.ipynb : contains a few constants and functions used in most of the algorithms.
+		
+
+* the other notebooks are the different assignments of these labs, look at lab_instructions for more information.
+
+### Running the labs
+
+* open a terminal
+
+* run "jupyter notebook"
+
+* in your web browser, open "lab_instructions.ipynb"
+
+* and follow instructions from there
+
+### Contact
+
+To contact me: Olivier.Sigaud@upmc.fr
---
+++ new_ipynb_utils/utils.py
@@ -0,0 +1,91 @@
+"""
+Utility functions for dealing with manipulating notebooks.
+
+Try to not put too many things here, nor to re-implement nbformat.
+"""
+import ast
+import re
+
+PREAMBLE=\
+"""
+##############################################################################
+# This source has been auto generated from an IPython/Jupyter notebook file. #
+# Please modify the origin file                                              #
+##############################################################################
+"""
+
+ALLOWED_NODES = set([
+    ast.ClassDef,
+    ast.FunctionDef,
+    ast.Import,
+    ast.ImportFrom
+])
+
+
+def validate_nb(nb):
+    """
+    Validate that given notebook JSON is importable
+
+    - Check for nbformat == 4
+    - Check that language is python
+
+    Do not re-implement nbformat here :D
+    """
+    if nb['nbformat'] != 4:
+        return False
+
+    language_name = (nb.get('metadata', {})
+        .get('kernelspec', {})
+        .get('language', '').lower())
+    return language_name == 'python'
+
+
+def filter_ast(module_ast):
+    """
+    Filters a given module ast, removing non-whitelisted nodes
+
+    It allows only the following top level items:
+     - imports
+     - function definitions
+     - class definitions
+     - top level assignments where all the targets on the LHS are all caps
+    """
+    def node_predicate(node):
+        """
+        Return true if given node is whitelisted
+        """
+        for an in ALLOWED_NODES:
+            if isinstance(node, an):
+                return True
+
+        # Recurse through Assign node LHS targets when an id is not specified,
+        # otherwise check that the id is uppercase
+        if isinstance(node, ast.Assign):
+            return all([node_predicate(t) for t in node.targets if not hasattr(t, 'id')]) \
+                and all([t.id.isupper() for t in node.targets if hasattr(t, 'id')])
+
+        return False
+
+    module_ast.body = [n for n in module_ast.body if node_predicate(n)]
+    return module_ast
+
+def code_from_ipynb(nb, markdown=False):
+    """
+    Get the code for a given notebook
+
+    nb is passed in as a dictionary that's a parsed ipynb file
+    """
+    code = PREAMBLE
+    for cell in nb['cells']:
+        if cell['cell_type'] == 'code':
+            # transform the input to executable Python
+            code += ''.join(cell['source'])
+        if cell['cell_type'] == 'markdown':
+            code += '\n# ' + '# '.join(cell['source'])
+        # We want a blank newline after each cell's output.
+        # And the last line of source doesn't have a newline usually.
+        code += '\n\n'
+    code = re.sub("(\n|^[\t ]*)%","\n#%",code)   
+    return code
+
+
